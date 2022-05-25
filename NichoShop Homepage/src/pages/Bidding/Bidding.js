b209c283d7d359a220f597c637da78b0ff104d81
Rachit <rachit@emizentech.com>
2022-05-25 18:12:10 +0530

    Bidding and Watchlist on Desktop

--- NichoShop Homepage/src/pages/Bidding/Bidding.js
+++ NichoShop Homepage/src/pages/Bidding/Bidding.js
@@ -48,13 +48,13 @@ class Pagination extends Component {
     productName(props){
         return (<div className='test'>
             <h4 className="mb-3"><a className="e-link" htef="">{props.first_name}- {props   .last_name}</a></h4>
-            <div><span className="text-black-50">Offered By:</span> <a className="e-link" htef="">seller username1</a></div>
-            <div><span className="text-black-50">Your max bid:</span> US $11.29</div>
+            <div><span className="text-black-50">Offered By:</span> <a className="e-link font-weight-medium" href="">seller username1</a></div>
+            <div><span className="text-black-50">Your max bid:</span> <span className="font-weight-medium">US $11.29</span></div>
         </div>);
     }    
     timeSection(props){
         return (<div className='timesection'>
-            <div className="left-time text-end">
+            <div className="left-time small-text18 text-end">
                 <span className="text-danger mb-3 d-block">12m 34s left</span>
                 <span className="d-block text-black-50">Today</span>
             </div> 
@@ -75,13 +75,13 @@ class Pagination extends Component {
                 <a className="buybutton btn btn-outline-primary" htef="">Buy it Now</a>
             </div>
             <div className="dropdown pt-3 pb-5">
-              <div className="dropdown-toggle" type="button" id="MoreActionsButton" data-bs-toggle="dropdown" aria-expanded="false">More actions</div>
+              <div className="dropdown-toggle small-text18 text-black-50" type="button" id="MoreActionsButton" data-bs-toggle="dropdown" aria-expanded="false">More actions</div>
               <ul className="dropdown-menu  dropdown-menu-end" aria-labelledby="MoreActionsButton">
                 <li><a className="dropdown-item" href="#">Add</a></li>
                 <li><a className="dropdown-item" href="#">Edit</a></li>
               </ul>
             </div>
-            <a className="remove-items text-danger" htef=""><i class="bi bi-trash"></i> Remove</a>
+            <a className="remove-items text-danger small-text18" htef=""><i class="bi bi-trash"></i> Remove</a>
         </div>);
     }      
    
@@ -98,14 +98,14 @@ class Pagination extends Component {
                         </Breadcrumbs>
                         <h2 className="title fw-bold my-4">Bidding</h2>
                         <div className="border rounded">
-                                <div className="dropdown top-dropdown pt-3 pb-5">
-                                    <span>Show:</span>
-                                    <div className="dropdown-toggle" type="button" id="total-records" data-bs-toggle="dropdown" aria-expanded="false">All(9)</div>
-                                    <ul className="dropdown-menu  dropdown-result-end" aria-labelledby="total-records">
-                                            <li><a className="dropdown-result" href="#">All(10)</a></li>
-                                            <li><a className="dropdown-result" href="#">All(20)</a></li>
-                                    </ul>
-                                </div> 
+                            <div className="dropdown top-dropdown p-3 small-text18 border-bottom d-flex justify-content-end">
+                                <div className="pe-2 text-black-50">Show:</div>
+                                <div className="dropdown-toggle font-weight-semibold" type="button" id="total-records" data-bs-toggle="dropdown" aria-expanded="false">All(9)</div>
+                                <ul className="dropdown-menu dropdown-result-end" aria-labelledby="total-records">
+                                        <li><a className="dropdown-item" href="#">All(10)</a></li>
+                                        <li><a className="dropdown-item" href="#">All(20)</a></li>
+                                </ul>
+                            </div> 
                             <table className="table m-0">
                                 <tbody>
                                     {
@@ -114,30 +114,30 @@ class Pagination extends Component {
                                             <tr>
                                                 <td width="180" className="p-3" scope="col">{this.gridTemplate(item)}</td>
                                                 <td scope="col" className="p-3">{this.productName(item)}</td>
-                                                <td width="10%" className="p-3" scope="col">{this.timeSection(item)} </td>
-                                                <td width="12%" className="p-3" scope="col">{this.priceSection(item)}</td>
-                                                <td width="15%" className="p-3" scope="col">{this.buttonSection(item)}</td>
+                                                <td className="p-3" scope="col">{this.timeSection(item)} </td>
+                                                <td className="p-3" scope="col">{this.priceSection(item)}</td>
+                                                <td className="p-3" scope="col">{this.buttonSection(item)}</td>
                                             </tr>
                                         )) :
                                         <h4>No Data Found!!</h4>
                                     }
                                 </tbody>
                             </table>
-                            <div className="biddingNote border-bottom p-3">*If you’ve been outbid, you can be again the high bidder in the case, if the seller cancels a bid or reduces the reserve price for example.</div> 
-                            <div className="p-3 d-flex justify-content-between align-items-center">
-                                <div className="page-result-total fs-5">Page 1 of 4</div>
+                            <div className="biddingNote border-bottom p-3 small-text18">*If you’ve been outbid, you can be again the high bidder in the case, if the seller cancels a bid or reduces the reserve price for example.</div> 
+                            <div className="px-3 py-4 d-flex justify-content-between align-items-center">
+                                <div className="page-result-total small-text18">Page 1 of 4</div>
                                 {totalRecords > 6 &&
                                     <PaginationComponent
                                         getAllData={this.data} 
                                         totalRecords={totalRecords}
                                         itemsCountPerPage = {limit} />
                                 }
-                                <div className="result-per-page d-flex">
-                                    <div className="pe-2 fs-6">Results per page</div>
-                                    <div className="dropdown-toggle" type="button" id="resultperpage" data-bs-toggle="dropdown" aria-expanded="false">20</div>
-                                    <ul className="dropdown-menu  dropdown-result-end" aria-labelledby="resultperpage">
-                                        <li><a className="dropdown-result" href="#">30</a></li>
-                                        <li><a className="dropdown-result" href="#">50</a></li>
+                                <div className="result-per-page d-flex align-items-center">
+                                    <div className="pe-2 small-text18">Results per page</div>
+                                    <div className="dropdown-toggle dropdown-blue px-2 small-text18" type="button" id="resultperpage" data-bs-toggle="dropdown" aria-expanded="false">20</div>
+                                    <ul className="dropdown-menu dropdown-result-end" aria-labelledby="resultperpage">
+                                        <li><a className="dropdown-item" href="#">30</a></li>
+                                        <li><a className="dropdown-item" href="#">50</a></li>
                                     </ul>
                                 </div>                                  
                             </div>
