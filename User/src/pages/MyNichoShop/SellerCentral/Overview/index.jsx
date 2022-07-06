8eb032de045d3911cb9c53235f95a225b3e83387
Shelina Developer <shelinadeveloper@gmail.com>
2022-07-06 23:05:20 +0500

    overview

--- User/src/pages/MyNichoShop/SellerCentral/Overview/index.jsx
+++ User/src/pages/MyNichoShop/SellerCentral/Overview/index.jsx
@@ -47,6 +47,88 @@ const SellerCentralOverview = () => {
                         <div className="ui-sco-head-name">
                             <p>Overview</p>
                         </div>
+                        <div className="d-flex justify-content-between">
+                            <div className="overview-box flex-grow-1 me-2">
+                                <div className="head p-3">
+                                    <h5 className="mb-0">Orders</h5>
+                                </div>
+                                <div className="body p-3">
+                                    <div className="d-flex justify-content-between">
+                                        <span className="text-danger">Not shipped</span>
+                                        <span>1,234</span>
+                                    </div>
+                                    <div className="d-flex justify-content-between">
+                                        <span>Awaiting shippment</span>
+                                        <span>0</span>
+                                    </div>
+                                    <div className="d-flex justify-content-between">
+                                        <span>Awaiting shippment</span>
+                                        <span>5</span>
+                                    </div>
+                                    <div className="d-flex justify-content-between">
+                                        <span>Buyer messages</span>
+                                        <span>0</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="overview-box flex-grow-1 me-2">
+                                <div className="head p-3">
+                                    <h5 className="mb-0">Requests</h5>
+                                </div>
+                                <div className="body p-3">
+                                    <div className="d-flex justify-content-between">
+                                        <span>Cancellation requests</span>
+                                        <span>1</span>
+                                    </div>
+                                    <div className="d-flex justify-content-between">
+                                        <span>Not received items</span>
+                                        <span>2</span>
+                                    </div>
+                                    <div className="d-flex justify-content-between">
+                                        <span>Return requests</span>
+                                        <span>12</span>
+                                    </div>
+                                    <div className="d-flex justify-content-between">
+                                        <span>Chargeback</span>
+                                        <span>0</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="overview-box flex-grow-1 me-2">
+                                <div className="head p-3">
+                                    <h5 className="mb-0">Listings</h5>
+                                </div>
+                                <div className="body p-3">
+                                    <div className="d-flex justify-content-between">
+                                        <span>Active listings</span>
+                                        <span>1234</span>
+                                    </div>
+                                    <div className="d-flex justify-content-between">
+                                        <span>Out of stock</span>
+                                        <span>5</span>
+                                    </div>
+                                    <div className="d-flex justify-content-between">
+                                        <span>Unsold items</span>
+                                        <span>123</span>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="overview-box flex-grow-1">
+                                <div className="head p-3">
+                                    <h5 className="mb-0">Seller perfomance</h5>
+                                </div>
+                                <div className="body p-3">
+                                    <div className="d-flex justify-content-between">
+                                        <span>Seller level</span>
+                                        <span className="text-success">Above standard</span>
+                                    </div>
+                                    <div className="d-flex justify-content-between">
+                                        <span>Positive feedback</span>
+                                        <span>98.7%</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </Col>
                 </Row>
             </Container>
