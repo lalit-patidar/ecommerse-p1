ef68dcfe7b7de1ce5f012ab3f2c83aa42f507155
Techinfomatic <53048119+Developmenteam30@users.noreply.github.com>
2022-07-26 19:13:16 +0530

    update

--- User/src/pages/MyNichoShop/Account/AccountUpdateYourEmailAddress/index.jsx
+++ User/src/pages/MyNichoShop/Account/AccountUpdateYourEmailAddress/index.jsx
@@ -1,8 +1,43 @@
+import React, { useEffect, useState } from "react";
 import { Button, TextField } from "@mui/material";
 // import "./accountchangeyourname.scss";
 import { ReactComponent as Logo } from "./../../../../assets/logo/logo.svg";
+import axios from "axios";
+import Toastify from "toastify-js";
+import "toastify-js/src/toastify.css";
 
 const AccountUpdateYourEmailAddress = () => {
+    const [userDetails, setUserDetails] = useState(null);
+    const [email, setemail] = useState(null);
+    const [oldemail, setoldemail] = useState(null);
+    const updateemail = async () => {
+        try{
+            var formdata = {
+                "userid":userDetails.id,
+                "newEmail":email
+            };
+            const data = await axios.put(
+                `${process.env.REACT_APP_API_DOMAIN}/account/changeEmail`,
+                formdata
+            );
+            toast('Updated successfully', "linear-gradient(to right, #00b09b, #96c93d)")
+        }catch(e){
+            toast(e.response.data.error, "linear-gradient(to right, #00b09b, #ff0000)")
+        }
+    }
+
+    const checkuser = async () => {
+        var u = await getLocalstore('_userLogin');
+        if(u && u.id){
+            setUserDetails(u);
+            setemail(u.email);
+        }
+    }
+
+    useEffect(()=>{
+        checkuser();
+    },[])
+
     return (
         <>
             <div className="ui-cyn-box">
@@ -32,6 +67,11 @@ const AccountUpdateYourEmailAddress = () => {
                                             fullWidth
                                             size="medium"
                                             type="email"
+                                            value={email}
+                                            onChange={event=>{
+                                                setemail(event.target.value);
+                                            }}
+
                                         />
                                     </div>
                                     <Button variant="contained">
